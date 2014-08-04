b432703428313830fa41ea048cc29027c5716faf
faiq <faiqrazarizvi@gmail.com>
2014-08-04 21:19:10 +0000

    added callback + testing now

---
+++ test.js
@@ -0,0 +1,12 @@
+var request = require('request') 
+
+var opts = { 
+  method: 'GET', 
+  uri: 'http://faiq-es-dev.internal.npmjs.com:9200/npm/package/LOLLOL', 
+  json: false 
+}
+
+request(opts, function(err, res){ 
+  if (err) console.error(err)
+  console.log(res.statuscode)   
+})
