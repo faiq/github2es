efa9a67234e4ef5435a6c5e60460b89b71a0e231
faiq <faiqrazarizvi@gmail.com>
2014-08-06 12:04:57 -0700

    got fake events emitting :)

--- tests/fakeApi.js
+++ tests/fakeApi.js
@@ -1,18 +1,30 @@
-var EE = require('events') 
+var EE = require('events').EventEmitter 
+  , fakeEvents = require('./fakeEvents') 
   , fakeFollow  = new EE
    
-setTimeout(fakeFollow.emit.bind(mock, "first-event", "some arg"), 0)
+/*setTimeout(fakeFollow.emit.bind(mock, "first-event", "some arg"), 0)
 setTimeout(fakeFollow.emit.bind(mock, "second-event"), 50)
 setTimeout(fakeFollow.emit.bind(mock, "third-event", 3), 100)
+*/
 
-function follow_feed(opts, cb) {
-  fakeFollow.on('change', function(ch) { return cb && cb.call(ch_feed, null, ch) });
+for (var c = 97; c < 'l'.charCodeAt(0); c++){
+  var letter = String.fromCharCode(c)
+  var randomTime = Math.floor((Math.random() * 1000) + 1)
+  setTimeout(fakeFollow.emit.bind(fakeFollow, "change", fakeEvents[letter] ), randomTime)
+}
 
-  process.nextTick(function() {
+//function follow(opts, cb) {
+  fakeFollow.on('change', function(ch){ 
+    console.log(ch)
+    //return cb && cb.call(ch_feed, null, ch) 
+  });
+
+  /*process.nextTick(function() {
     ch_feed.follow();
   })
 
-  return ch_feed;
-}
+  return ch_feed;*/
+//}
 
-module.exports = follow_feed;
+//module.exports = follow
+
