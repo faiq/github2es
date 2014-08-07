172db458474f24afed595081c2e28bbe851f52d3
faiq <faiqrazarizvi@gmail.com>
2014-08-06 19:02:14 -0700

    PASSING TESTS WITH JANKY ASS CODE

--- tests/fakeApi.js
+++ tests/fakeApi.js
@@ -1,30 +1,29 @@
 var EE = require('events').EventEmitter 
   , fakeEvents = require('./fakeEvents') 
   , fakeFollow  = new EE
-   
-/*setTimeout(fakeFollow.emit.bind(mock, "first-event", "some arg"), 0)
-setTimeout(fakeFollow.emit.bind(mock, "second-event"), 50)
-setTimeout(fakeFollow.emit.bind(mock, "third-event", 3), 100)
-*/
 
-for (var c = 97; c < 'l'.charCodeAt(0); c++){
-  var letter = String.fromCharCode(c)
-  var randomTime = Math.floor((Math.random() * 1000) + 1)
-  setTimeout(fakeFollow.emit.bind(fakeFollow, "change", fakeEvents[letter] ), randomTime)
+function startEvents (){   
+  for (var c = 97; c < 'l'.charCodeAt(0); c++){
+    var letter = String.fromCharCode(c)
+    var randomTime = Math.floor((Math.random() * 1000) + 1)
+    setTimeout(fakeFollow.emit.bind(fakeFollow, "change", fakeEvents[letter] ), randomTime)
+  }
+} 
+
+fakeFollow.pause = function (){ 
+  console.log('pause')
+} 
+
+fakeFollow.resume = function (){ 
+  console.log('resume') 
+} 
+
+function follow(opts, cb) {
+  startEvents()
+  fakeFollow.on('change', function(ch){ 
+    cb(null, ch)   
+  })
+  return fakeFollow
 }
 
-//function follow(opts, cb) {
-  fakeFollow.on('change', function(ch){ 
-    console.log(ch)
-    //return cb && cb.call(ch_feed, null, ch) 
-  });
-
-  /*process.nextTick(function() {
-    ch_feed.follow();
-  })
-
-  return ch_feed;*/
-//}
-
-//module.exports = follow
-
+module.exports = follow
--- tests/testg2e.js
+++ tests/testg2e.js
@@ -8,18 +8,19 @@ var lab = require('lab')
   , describe = lab.describe
   , it = lab.it
   , before = lab.before
+  , beforeEach = lab.beforeEach
+  , afterEach = lab.afterEach
   , github2es = require('../github2es')
   , fs = require('fs')
   , path = require('path')
   , expect = lab.expect  
   , sampleAllDocs = require('./mocks/all-docs-mock.json')
   , nockCalls = require('./nock-calls')
+  , follow = require('./fakeApi') 
   , async = require('async')
   , redis = require('redis')
   , spawn = require('child_process').spawn
   , nock = require('nock'); 
-
-  
 
 var fakeGitCalls;
 var fakeAll = sampleAllDocs.rows;
@@ -36,13 +37,13 @@ function NockFactory(Package) {
   
 function NockFactoryOctonode(Package) { 
   nock(Package.host).get(Package.path + '?access_token=' + process.env.githubApi).times(Package.times).reply(Package.statusCode || 200, Package.file); 
-} 
- 
+}
+
 function makeCalls ()  {
   var octonodeCalls = [];
   var githubCalls = []; 
   var registryCalls = [];
-  
+  var elasticsearchCalls = [];
   var oc = 0; 
   Object.keys(octonodeObj).forEach(function (Package){
     octonodeCalls[oc] = NockFactoryOctonode(octonodeObj[Package]);  
@@ -55,18 +56,47 @@ function makeCalls ()  {
     gc++
   });
   fakeGitCalls = gc; 
+  
   var rc = 0; 
   Object.keys(registryObj).forEach(function (Package){
     registryCalls[rc] = NockFactory(registryObj[Package]); 
     rc++; 
-  }); 
+  });
+  
+  var yo = 0; 
+  var packageUrls = ['28','request', 'Frog', 'express', 'JSON'
+                    ,'osxh', 'voodoo', 'vargs-callback', 'vector2d'];
+    
+  packageUrls.forEach(function(p){   
+    var statCode;
+    var res; 
+    if (p === '28'){ 
+      statCode = 404; 
+      res = {statusCode: 404} 
+    }
+    else{ 
+      statCode = 200
+      res = 'OK' 
+    } 
+    elasticsearchCalls[yo] = nock('http://localhost').filteringRequestBody(function(path) {
+        return '*';
+    }).post('/npm/package/' + p + '/_update', '*')
+    .reply(statCode, res);
+    yo++;
+  });
 }
+
+/*var fuck = nock('http://localhost').filteringRequestBody(function(path) {
+      return '*';
+    }).post('/npm/package/Frog/_update', '*')
+      .reply(200, 'OK (excpet for one)');*/
+
 makeCalls();
 var fakeES = nock('http://localhost:9200').get('/npm').reply(200, 'Fake ES stuff');
 
-var followerAll = new github2es('http://loa', process.env.githubApi, path.join(__dirname , 'sequence.seq'));   
 
 describe('github2es constrctor', function () {
+  
   it('needs an api parameter', function(done){ 
     expect(noApi).to.throw(Error);
     done();  
@@ -76,46 +106,51 @@ describe('github2es constrctor', function () {
   });
  
 });
-
-describe('processing the functions (getting metadata -> posting ES)'), {timeout: 7000}, function (){ 
+ 
+describe('processing the functions (getting metadata -> posting ES)', {timeout: 7000}, function (){ 
   //fire up a redis server, put things into it that I want, see if they behave the way I want them to 
   var client; 
   var startRedis;
-  before(function (done){ 
+  var opts = {}; 
+  opts.follow = follow;
+  
+  beforeEach(function (done){ 
     startRedis = spawn('redis-server')
     setTimeout(function(){ 
-      client = redis.createClient()
-      client.flushall()
-      
-    }, 2000) 
+      console.log('before') 
+      client = redis.createClient();
+      client.flushall(); 
+      fs.unlinkSync(path.join(__dirname , 'sequence.seq'));
+      done();
+    }, 3000); 
   });   
-
-
-
-
-
-  it('processes the worksize amount of packages at a time', function(done){
-    var follower = new github2es(fakeAll, fakeES, process.env.githubApi, path.join(__dirname , 'sequence2.seq'), function () {console.log('done with all the packages'); }); 
-    var asyncArr = follower.makeFuncs();
-    lab.expect(asyncArr.length).to.equal(follower.workSize);
-    done();      
-  });
- 
+  
   it('gets the appropriate info from github', function(done) { 
-    var follower2 = new github2es(fakeAll, fakeES, process.env.githubApi, path.join(__dirname , 'sequence3.seq'), function () {console.log('done with all the packages'); } );   
+    var follower2 = new github2es('http://localhost/npm', 'http://localhost:15984/registry', process.env.githubApi, 'packages', 259200, path.join(__dirname , 'sequence.seq'), opts);
     makeGithub(); 
     function makeGithub(){
       var packageUrls = ['28msec/28', 'spiceapps/cashew', 'mikeal/request', 'kaisellgren/Frog', 'visionmedia/express', 'douglascrockford/JSON-js'
                         ,'nodejitsu/http-server', 'proximitybbdo/voodoo', 'furagu/vargs-callback', 'evanshortiss/vec2d']; 
+      console.log(packageUrls.length);
       packageUrls.forEach(function (p) { 
           follower2.getGithubInfo(p, p.substring('/'), function (err, res){
-            console.log('on this package ' + p);
-            if(p.substring('/') === 'cashew'){expect(res).to.equal(null); }
-            else {  expect(err).to.equal(null) }
+            if (p.substring(p.indexOf('/') + 1) === 'cashew') expect(typeof(err)).to.equal('object'); 
+            else expect(err).to.equal(null); 
           });  
       }); 
-    done();  
-  }
+    }
+    setTimeout(function(){ 
+      done()
+    }, 3000)
   });
- 
+  
+  it('tries to retry on a broken package with an updated timestamp', function(done){
+    var follower2 = new github2es('http://localhost/npm', 'http://localhost:15984/registry', process.env.githubApi, 'packages', 259200, path.join(__dirname , 'sequence.seq'), opts);
+    follower2.esPost('28', {fake:'yo'}, function (err, res){
+      console.log(err)
+      expect(err).to.equal(null);
+      expect(typeof(res)).to.equal('string'); 
+      done(); 
+    });  
+    }) 
 });
